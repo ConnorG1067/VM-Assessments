6dd7fd47a7d1b6f626ea1ee413762e2e41461c39
ConnorG1067 <concon1067@gmail.com>
2022-09-23 16:13:29 -0400

    .

--- Tutorial/Tutorial2/divisor.c
+++ Tutorial/Tutorial2/divisor.c
@@ -3,13 +3,17 @@
 int gcd(int,int);
 
 int main(void){
-
-    for(int i = 1; i<15; i++){
-        for(int j = 1; j<15; j++){
-            int greatestCommonDivisor = gcd(i,j);
-            printf("GCD for numbers %d, %d: %d\n",i,j,greatestCommonDivisor);   
-        }
-    }
+	//TEST VALUES
+	printf("GCD of 5 & 3 (expected 1): %d\n", gcd(5,3)); 
+	printf("GCD of 3 & 5 (expected 1): %d\n", gcd(3,5)); 
+	printf("GCD of 10 & 2 (expected 2): %d\n", gcd(10,2)); 
+	printf("GCD of 5 & 5 (expected 5): %d\n", gcd(5,5)); 
+	printf("GCD of 6 & 3 (expected 3): %d\n", gcd(6,3)); 
+	printf("GCD of 1 & 5 (expected 1): %d\n", gcd(1,5)); 
+	printf("GCD of 100 & 10 (expected 10): %d\n", gcd(100,10)); 
+	printf("GCD of 1000000 & 100 (expected 100): %d\n", gcd(1000000,100)); 
+	printf("GCD of 72 & 4 (expected 4): %d\n", gcd(72, 4)); 
+	
     return 0;
 }
 

6ffb4f3005e702b957fcf9303f6822d524f098f2
ConnorG1067 <concon1067@gmail.com>
2022-10-06 15:06:30 -0400

    .

--- Assignments/Assignment2/Assignment2.c
+++ Assignments/Assignment2/Assignment2.c
@@ -17,12 +17,15 @@ unsigned char decryptByte(unsigned char, unsigned char, unsigned char);
 unsigned char getBit(unsigned char, int);
 unsigned char setBit(unsigned char, int);
 unsigned char clearBit(unsigned char, int);
-
+void printBits(unsigned char);
 
 int main()
 {
   char str[8];
   int  choice;
+	
+  unsigned char encryptMsg[MAX_BUF];
+  unsigned char cipherMsg[MAX_BUF];
 
   printf("\nYou may:\n");
   printf("  (1) Encrypt a message \n");
@@ -37,21 +40,30 @@ int main()
 
   switch (choice) {
     case 1:
-	  char encryptMsg[MAX_BUF];
-      char cipherMsg[MAX_BUF];
-
 	  fgets(encryptMsg, sizeof(encryptMsg), stdin);
-        
       encode(encryptMsg, cipherMsg, sizeof(encryptMsg));
       break;
 
     case 2:
+	  printf("\n  Use \"-1\" to end your message to decrypt\n  >");
+	  int arrayCounter = 0;
+	  unsigned int currentValue;
+
+	  while(1){
+		scanf("%u", &currentValue);
+		if(currentValue == -1){
+			break;
+		}
+		cipherMsg[arrayCounter++] = currentValue; 	
+	  }
+      decode(cipherMsg, encryptMsg, arrayCounter);
 
       break;
   }
 
   return(0);
 }
+
 
 /*
   Function:  getBit
@@ -94,13 +106,11 @@ unsigned char processCtr(unsigned char ctr, unsigned char key){
 	int tempCounter = ctr;
 	for(int i = (tempCounter%2 == 0) ? 0 : 1; i<8; i+=2){
         int xorVal = getBit(ctr, i)^getBit(key, i);
-        //printf("%u, %u = %u\n", getBit(ctr, i), getBit(key, i), xorVal);
-        //If the xorVal is 0 clear the bit at the position if it is not 0 then set the bit
 		tempCounter = (xorVal == 0) ? clearBit(tempCounter, i) : setBit(tempCounter,i);
 	} 
 	return tempCounter;
 }
-//Ctr = counter, pt = plain text, prev = previous 
+
 unsigned char encryptByte(unsigned char pt, unsigned char ctr, unsigned char prev){
 	int tempByte = 0;
 
@@ -117,29 +127,47 @@ unsigned char encryptByte(unsigned char pt, unsigned char ctr, unsigned char pre
 	return tempByte;
 }
 
-//Review this shit show of a function
 void encode(unsigned char* pt, unsigned char* ct, int numBytes){
     int i = 0; 
-    int counter = processCtr(CTR, KEY);
+    int counter; 
 
     do{
-		//Testing values
-		printf("%c, ", pt[i]);
-		printf("%d: ", i);
+		counter = processCtr((i==0) ? CTR : counter, KEY);
         ct[i] = encryptByte(pt[i], counter, (i == 0) ? IV : ct[i-1]);
-        printf("%u\n", ct[i]);
+        printf("%u ", ct[i]);
         counter++;
         i++;
     }while(pt[i+1] != '\0');
 }
 
 unsigned char decryptByte(unsigned char ct, unsigned char ctr, unsigned char prev){
+	int tempByte = 0;
 
+	for(int i = 0; i<8; i++){
+		if(getBit(ctr, i) == 1){
+            int xorVal = getBit(ct, i)^getBit(prev, i);
+			tempByte = (xorVal == 0) ? clearBit(tempByte, i) : setBit(tempByte, i);
+		}else{
+            int xorVal = getBit(ct, i)^getBit(prev, 7-i);
+			tempByte = (xorVal == 0) ? clearBit(tempByte, i) : setBit(tempByte, i);
+        }
+	}
 
+	return tempByte;
 }
 
 
-//Note we read our bits from right to left
-//
-//
+void decode(unsigned char *ct, unsigned char *pt, int numBytes){
+	int i = 0; 
+    int counter = CTR;
+
+    do{
+		counter = processCtr(counter, KEY);
+        pt[i] = decryptByte(ct[i], counter, (i == 0) ? IV : ct[i-1]);
+        printf("%c ", pt[i]);
+        counter++;
+        i++;
+    }while(i < numBytes);
+}
+
 
